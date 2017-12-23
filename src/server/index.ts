b8a3d66cc4b796c36230fe0ba9f3efa698a39da5
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2017-12-23 11:13:27 +0100

    first commit of new release

---
+++ src/server/index.ts
@@ -0,0 +1,11 @@
+import * as akala from '@akala/server';
+import * as va from '@domojs/chat'
+import * as vm from 'vm';
+import * as url from 'url';
+import * as path from 'path';
+var debug = akala.log('domojs:chat');
+
+akala.worker.createClient('chat').then(function (client)
+{
+    va.meta.createServerProxy(client).register({ path: path.resolve(__dirname, './interpreter'), name: 'utilities' });
+});
