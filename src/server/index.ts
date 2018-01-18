e5c49a30bbee695b36e4745912f37f4a8ab49915
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-01-18 07:07:03 +0100

    restored too many deleted files

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,9 @@
 import * as akala from '@akala/server';
-import * as va from '@domojs/chat'
 import * as vm from 'vm';
 import * as url from 'url';
 import * as path from 'path';
+import * as va from './language';
+
 var debug = akala.log('domojs:chat');
 
 akala.worker.createClient('chat').then(function (client)
