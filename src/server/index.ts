0794775ec02987ca052258b25078d5612995bad6
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-03-09 08:51:08 +0100

    cleanup files

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,9 +3,9 @@ import * as va from '@domojs/chat'
 import * as vm from 'vm';
 import * as url from 'url';
 import * as path from 'path';
-var debug = akala.log('domojs:chat');
+var debug = akala.log('domojs:chat:date');
 
 akala.worker.createClient('chat').then(function (client)
 {
-    va.meta.createServerProxy(client).register({ path: path.resolve(__dirname, './interpreter'), name: 'utilities' });
+    va.meta.createServerProxy(client).register({ path: path.resolve(__dirname, './interpreter'), name: 'date' });
 });
